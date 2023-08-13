96253f4db0cf478be5170d6da4606a1151d02236
Desmond Ademeso <130741918+KingDez04@users.noreply.github.com>
2023-08-13 22:25:33 +0100

    Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,13 +3,12 @@ import React from 'react';
 import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
-import { Span } from './HeaderStyles';
 
 const Hero = (props) => (
   <Section row nopadding>
     <LeftSection>
       <SectionTitle main center >
-        <Span>HELLO, WORLD.</Span><br />
+        <p>style={{color: 'yellow'}}>HELLO, WORLD.</p><br />
         I'm Ademeso, Ademola.
       </SectionTitle>
       <SectionText>
