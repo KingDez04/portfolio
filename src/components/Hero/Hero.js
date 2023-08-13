ac175da875069ec55443bf68ac7e9a4b6df642b0
Desmond Ademeso <130741918+KingDez04@users.noreply.github.com>
2023-08-13 22:16:10 +0100

    Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
+import { Span } from './HeaderStyles';
 
 const Hero = (props) => (
   <Section row nopadding>
